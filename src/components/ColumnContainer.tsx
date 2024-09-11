757f7826edb0f4db9188c19e0682cbfdabab7bd6
npq-thien <thiennguyen3132002@gmail.com>
2024-09-11 22:20:26 +0700

    fixed render UI wrong when dragging task in 1 column

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,7 +1,11 @@
 import { CSS } from "@dnd-kit/utilities";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import { FaPlus } from "react-icons/fa";
-import { SortableContext, useSortable } from "@dnd-kit/sortable";
+import {
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 
 import { Column, Id, Task, TaskActivity } from "../constants/types";
 import TaskCard from "./TaskCard";
@@ -26,7 +30,6 @@ const ColumnContainer = (props: Props) => {
 
   const role = useSelector((state: RootState) => state.auth.role);
 
-  
   const sortedTasks = useMemo(() => {
     return tasks.slice().sort((a, b) => a.position - b.position);
   }, [tasks]);
@@ -34,7 +37,7 @@ const ColumnContainer = (props: Props) => {
   const taskIds = useMemo(() => {
     return sortedTasks.map((task) => task.id);
   }, [sortedTasks]);
-  
+
   // console.log('in column', tasks)
   // console.log('sorted tasks', sortedTasks)
   // console.log('sorted ids', taskIds)
@@ -55,18 +58,22 @@ const ColumnContainer = (props: Props) => {
   return (
     <div
       ref={setNodeRef}
-      style={style}
+      // style={style}
       // className="w-[280px] max-h-[500px] overflow-y-auto flex flex-col gap-4 bg-gradient-to-b from-cream-4 to-[rgba(255,255,255,0.1)] rounded-lg p-2"
       className="w-[280px] max-h-[500px] overflow-y-auto flex flex-col gap-4 bg-cream-3 rounded-xl px-2 pb-4"
       id={column.id.toString()}
     >
-      <header className="sticky top-0 flex-between gap-2 font-bold py-2 bg-cream-3">
+      <header className="sticky top-0 flex-between gap-2 font-bold pt-2 mb-2 bg-cream-3">
         <div className="flex gap-2 items-center ml-2">
           {column.title}
           <p className="rounded-full bg-light-1 px-2">{tasks.length}</p>
         </div>
       </header>
-      <SortableContext items={taskIds} id={column.status}>
+      <SortableContext
+        items={taskIds}
+        id={column.status}
+        strategy={verticalListSortingStrategy}
+      >
         <div className="flex flex-col gap-2">
           {sortedTasks.map((task) => (
             <TaskCard
